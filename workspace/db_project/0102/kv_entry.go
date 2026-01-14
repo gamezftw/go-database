21b8752c4de2155cf480b548ef2c353ce789221b
Your Name <you@example.com>
2026-01-14 14:04:58 +0100

    refactor(0102): use writer for encoding

--- workspace/db_project/0102/kv_entry.go
+++ workspace/db_project/0102/kv_entry.go
@@ -1,7 +1,9 @@
 package db0102
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
 )
 
@@ -10,33 +12,32 @@ type Entry struct {
 	val []byte
 }
 
-func (ent *Entry) Encode() []byte {
-	keyLen := len(ent.key)
-	valLen := len(ent.val)
-	data := make([]byte, 4+4+keyLen+valLen)
-	binary.LittleEndian.PutUint32(data[0:4], uint32(keyLen))
-	binary.LittleEndian.PutUint32(data[4:8], uint32(valLen))
-	copy(data[8:], ent.key)
-	copy(data[8+keyLen:], ent.val)
-	return data
+func (ent *Entry) Encode() io.Writer {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, uint32(len(ent.key)))
+	binary.Write(buf, binary.LittleEndian, uint32(len(ent.val)))
+	buf.Write(ent.key)
+	buf.Write(ent.val)
+
+	return buf
 }
 
 func (ent *Entry) Decode(r io.Reader) error {
 	keyLenByte := make([]byte, 4)
-	r.Read(keyLenByte)
+	_, keyLenError := r.Read(keyLenByte)
 
 	valLenByte := make([]byte, 4)
-	r.Read(valLenByte)
+	_, valLenError := r.Read(valLenByte)
 
 	keyData := make([]byte, binary.LittleEndian.Uint32(keyLenByte))
-	r.Read(keyData)
+	_, keyDataError := r.Read(keyData)
 	ent.key = keyData
 
 	valData := make([]byte, binary.LittleEndian.Uint32(valLenByte))
-	r.Read(valData)
+	_, valDataError := r.Read(valData)
 	ent.val = valData
 
-	return nil
+	return errors.Join(keyLenError, valLenError, keyDataError, valDataError)
 }
 
 // QzBQWVJJOUhU https://trialofcode.org/
--- workspace/db_project/0102/kv_test.go
+++ workspace/db_project/0102/kv_test.go
@@ -34,12 +34,13 @@ func TestKVBasic(t *testing.T) {
 
 func TestEntryEncode(t *testing.T) {
 	ent := Entry{key: []byte("k1"), val: []byte("xxx")}
-	data := []byte{2, 0, 0, 0, 3, 0, 0, 0, 'k', '1', 'x', 'x', 'x'}
+	data := new(bytes.Buffer)
+	data.Write([]byte{2, 0, 0, 0, 3, 0, 0, 0, 'k', '1', 'x', 'x', 'x'})
 
 	assert.Equal(t, data, ent.Encode())
 
 	decoded := Entry{}
-	err := decoded.Decode(bytes.NewBuffer(data))
+	err := decoded.Decode(data)
 	assert.Nil(t, err)
 	assert.Equal(t, ent, decoded)
 }

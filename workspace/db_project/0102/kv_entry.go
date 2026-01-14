802969dc806f12d43dee2eed3c401fb53bfabb71
Your Name <you@example.com>
2026-01-14 14:04:58 +0100

    refactor(0102): use writer for encoding

--- workspace/db_project/0102/kv_entry.go
+++ workspace/db_project/0102/kv_entry.go
@@ -1,7 +1,9 @@
 package db0102
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
 )
 
@@ -11,32 +13,35 @@ type Entry struct {
 }
 
 func (ent *Entry) Encode() []byte {
-	keyLen := len(ent.key)
-	valLen := len(ent.val)
-	data := make([]byte, 4+4+keyLen+valLen)
-	binary.LittleEndian.PutUint32(data[0:4], uint32(keyLen))
-	binary.LittleEndian.PutUint32(data[4:8], uint32(valLen))
-	copy(data[8:], ent.key)
-	copy(data[8+keyLen:], ent.val)
-	return data
+	keyLen := uint32(len(ent.key))
+	valLen := uint32(len(ent.val))
+
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.LittleEndian, keyLen)
+	binary.Write(buf, binary.LittleEndian, valLen)
+	buf.Write(ent.key)
+	buf.Write(ent.val)
+
+	return buf.Bytes()
 }
 
 func (ent *Entry) Decode(r io.Reader) error {
 	keyLenByte := make([]byte, 4)
-	r.Read(keyLenByte)
+	_, keyLenError := r.Read(keyLenByte)
 
 	valLenByte := make([]byte, 4)
-	r.Read(valLenByte)
+	_, valLenError := r.Read(valLenByte)
 
 	keyData := make([]byte, binary.LittleEndian.Uint32(keyLenByte))
-	r.Read(keyData)
+	_, keyDataError := r.Read(keyData)
 	ent.key = keyData
 
 	valData := make([]byte, binary.LittleEndian.Uint32(valLenByte))
-	r.Read(valData)
+	_, valDataError := r.Read(valData)
 	ent.val = valData
 
-	return nil
+	return errors.Join(keyLenError, valLenError, keyDataError, valDataError)
 }
 
 // QzBQWVJJOUhU https://trialofcode.org/

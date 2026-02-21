106b4d139cffd3cf012f12cd449259f784e78426
Your Name <you@example.com>
2026-02-21 20:01:29 +0100

    0201

--- workspace/db_project/0201/cell.go
+++ workspace/db_project/0201/cell.go
@@ -1,5 +1,9 @@
 package db0201
 
+import (
+	"encoding/binary"
+)
+
 type CellType uint8
 
 const (
@@ -13,8 +17,30 @@ type Cell struct {
 	Str  []byte
 }
 
-func (cell *Cell) Encode(toAppend []byte) []byte
+func (cell *Cell) Encode(toAppend []byte) []byte {
+	switch cell.Type {
+	case TypeI64:
+		toAppend = binary.LittleEndian.AppendUint64(toAppend, uint64(cell.I64))
+	case TypeStr:
+		toAppend = binary.LittleEndian.AppendUint32(toAppend, uint32(len(cell.Str)))
+		toAppend = append(toAppend, cell.Str...)
+	}
+	return toAppend
+}
 
-func (cell *Cell) Decode(data []byte) (rest []byte, err error)
+func (cell *Cell) Decode(data []byte) (rest []byte, err error) {
+	switch cell.Type {
+	case TypeI64:
+		cell.I64 = int64(binary.LittleEndian.Uint64(data[:8]))
+		return data[8:], nil
+	case TypeStr:
+		var length = binary.LittleEndian.Uint32(data[:4])
+		var endIndex = 4 + length
+		cell.Str = data[4:endIndex]
+		return data[endIndex:], nil
+	}
+
+	return rest, err
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/

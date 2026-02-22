a5a8ececdc43f6f051c9c6298c0b9c50576d8399
Your Name <you@example.com>
2026-02-22 21:06:09 +0100

    0202

--- workspace/db_project/0202/row.go
+++ workspace/db_project/0202/row.go
@@ -1,5 +1,9 @@
 package db0202
 
+import (
+	"slices"
+)
+
 type Schema struct {
 	Table string
 	Cols  []Column
@@ -17,12 +21,49 @@ func (schema *Schema) NewRow() Row {
 	return make(Row, len(schema.Cols))
 }
 
-func (row Row) EncodeKey(schema *Schema) (key []byte)
+func (row Row) EncodeKey(schema *Schema) (key []byte) {
+	key = append(key, []byte(schema.Table)...)
+	key = append([]byte(schema.Table), 0x00)
+	for _, col := range schema.PKey {
+		key = row[col].Encode(key)
+	}
+	return key
+}
 
-func (row Row) EncodeVal(schema *Schema) (val []byte)
+func (row Row) EncodeVal(schema *Schema) (val []byte) {
+	for colIndex := range schema.Cols {
+		if !slices.Contains(schema.PKey, colIndex) {
+			val = row[colIndex].Encode(val)
+		}
+	}
 
-func (row Row) DecodeKey(schema *Schema, key []byte) (err error)
+	return val
+}
 
-func (row Row) DecodeVal(schema *Schema, val []byte) (err error)
+func (row Row) DecodeKey(schema *Schema, key []byte) (err error) {
+	key = key[len(schema.Table)+1:]
+	for _, colIndex := range schema.PKey {
+		row[colIndex].Type = schema.Cols[colIndex].Type
+		key, err = row[colIndex].Decode(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (row Row) DecodeVal(schema *Schema, val []byte) (err error) {
+	for colIndex := range schema.Cols {
+		if !slices.Contains(schema.PKey, colIndex) {
+			row[colIndex].Type = schema.Cols[colIndex].Type
+			val, err = row[colIndex].Decode(val)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
